38f27a6164ed793d56d93cdb8a7c6613617ae216
Irfaan Osman <49390798+Ifoo777@users.noreply.github.com>
2022-12-29 00:21:28 +0200

    L4T21 Capstone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Hangman from './Hangman';
+import Help from './Help';
 
+/*Display the Hangman game and Help button*/
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><u>Hangman Game</u></h1>
+      <br/>
+      <Hangman/> 
+      <Help/> 
+      <br/><br/>    
     </div>
   );
 }
